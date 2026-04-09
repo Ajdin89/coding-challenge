394a305963b4cce3221e42f64c5b7cff5cea64ba
Ajdin Kmetas <ajdinkmetas@outlook.com>
2026-04-09 02:19:54 +0200

    Add PrismaModule and PrismaService for database integration

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService implements OnModuleInit, OnModuleDestroy {
+  private client: PrismaClient;
+
+  constructor() {
+    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+    this.client = new PrismaClient({ adapter });
+  }
+
+  get event() {
+    return this.client.event;
+  }
+
+  async onModuleInit() {
+    await this.client.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.client.$disconnect();
+  }
+}
