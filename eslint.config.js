7b80d3c6c9f39615d56858d7fc8f9e4fe14a7f2d
Ajdin Kmetas <ajdinkmetas@outlook.com>
2026-04-10 01:39:27 +0200

    Update ESLint configurations and bug fixes

--- eslint.config.js
+++ eslint.config.js
@@ -3,15 +3,35 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
+    files: ['backend/**/*.{ts,tsx}'],
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.node,
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['backend/tsconfig.json'],
+      },
+    },
+  },
+  {
+    files: ['frontend/**/*.{ts,tsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['frontend/tsconfig.json'],
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
